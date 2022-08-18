6d8a7209c161d4fce99ab59c561d4d22bdaa8cae
Sujankandel58 <110717785+Sujankandel58@users.noreply.github.com>
2022-08-18 18:08:44 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// this is the console application.
 namespace ConsoleApplication
 {
     class Program
